9a94251360a85140c8444d0ecb0ba054e03607c8
yuesth <yues.963@gmail.com>
2021-06-08 20:09:40 +0700

    added sitemap

--- server.js
+++ server.js
@@ -12,6 +12,56 @@ app.use(express.static(path.resolve(__dirname, './build')));
 //   response.sendFile(filePath);
 // });
 
+app.get("/sitemap", (req, res) => {
+    res.setHeader("Content-Type", "text/xml")
+    res.write(`
+    <?xml version="1.0" encoding="UTF-8"?>
+<urlset
+      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+<!-- created with Free Online Sitemap Generator www.xml-sitemaps.com -->
+<url>
+  <loc>https://aqlpeduli.or.id/</loc>
+  <lastmod>2020-12-23T06:53:47+00:00</lastmod>
+  <priority>1.00</priority>
+</url>
+<url>
+  <loc>https://aqlpeduli.or.id/kepedulian</loc>
+  <lastmod>2020-12-23T06:53:47+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>https://aqlpeduli.or.id/relawan</loc>
+  <lastmod>2020-12-23T06:53:47+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>https://aqlpeduli.or.id/profil</loc>
+  <lastmod>2020-12-23T06:53:47+00:00</lastmod>
+  <priority>0.60</priority>
+</url>
+<url>
+  <loc>https://aqlpeduli.or.id/berita</loc>
+  <lastmod>2020-12-23T06:53:47+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>https://aqlpeduli.or.id/informasi</loc>
+  <lastmod>2020-12-23T06:53:47+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>https://aqlpeduli.or.id/khazanah</loc>
+  <lastmod>2020-12-23T06:53:47+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+</urlset>
+    `)
+    res.end()
+})
+
 app.get("/", (req, response) => {
     const filePath = path.resolve(__dirname, './build', 'index.html')
     fs.readFile(filePath, 'utf8', function (err, data) {
